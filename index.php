32c2962b71e5a2487fc7efe62043f2f8b5a8ce70
MoonGi-Kim <Jkim5918@conestogac.on.ca>
2020-02-19 13:27:50 -0500

    init new sources
    init srcs

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>CPA Programs</title>
+		<link rel="stylesheet" href="styles/index.css">
+		<link rel="stylesheet" href="styles/main.css">
+		<link rel="icon" type="image/ico" href="images/moon_logo3.png">
+	</head>
+	<body>
+		<header>
+			<img src="images/moon_logo2.png" alt="logo" width="60" height="60">
+			<h1>CPA Programs</h1>
+			<h3>All about it - <em class="shadow">First </em> Semester</h3>
+		</header>
+		<nav id="nav_main">
+			<ul>
+				<li><a href="index.html" class="current">Home</a></li>
+				<li><a href="pages/prog1245.html">PROG1245</a>
+					<ul>
+						<li><a href="#">Introduction</a></li>
+						<li><a href="#">Contents</a>
+							<ul>
+								<li><a href="#">Week 1</a></li>
+								<li><a href="#">Week 2</a></li>
+								<li><a href="#">Week 3</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="pages/prog1780.html">PROG1780</a></li>
+				<li><a href="pages/math1910.html">MATH1910</a></li>
+				<li><a href="pages/contact.html">Contact US</a></li>
+			</ul>
+		</nav>
+		<aside id="sideA">
+			<nav id="nav_sideA">
+				<ul>
+					<li><a href="index.html"><img src="images/schoolbus2.jpg" alt="schoolbus2"></a><a href="index.html" class="current">Home</a></li>
+					<li><a href="pages/prog1245.html"><img src="images/PROG1245.jpg" alt="PROG1245"></a><a href="pages/prog1245.html">PROG1245</a></li>
+					<li><a href="pages/prog1780.html"><img src="images/prog1780.jpg" alt="prog1780"></a><a href="pages/prog1780.html">PROG1780</a></li>
+					<li><a href="pages/math1910.html"><img src="images/math1910.jpg" alt="maht1910"></a><a href="pages/math1910.html">MATH1910</a></li>
+					<li><a href="pages/contact.html"><img src="images/moon_logo1.png" alt="logo"></a><a href="pages/contact.html">Contact Us</a></li>
+				</ul>
+			</nav>
+		</aside>
+		<main>
+			<section>
+				<h1>Welcome to My Site</h1>
+				<h2>Please enjoy my page.</h2>
+				<hr/>
+				<p>This screen will show you the programs that you are enrolled in and the courses associated with each program.</p>
+				<p>If you have questions, or need associated with your curriculum, and are enrolled in a full time program, please contact us.</p>
+				<hr/>
+				<table>
+					<caption>Academic List</caption>
+					<thead>
+						<tr>
+							<th>Program ID</th>
+							<th>Section</th>
+							<th>Title of Subject</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>PROG1245</td>
+							<td>Section 1</td>
+							<td>Programming Web Fundamentals</td>
+						</tr>
+						<tr>
+							<td>PROG1780</td>
+							<td>Section 1</td>
+							<td>Programming Fundamentals</td>
+						</tr>
+						<tr>
+							<td>MATH1910</td>
+							<td>Section 10</td>
+							<td>Mathematics for information Technology II</td>
+						</tr>
+					</tbody>
+				</table>
+				<hr/>
+			</section>
+		</main>
+		<aside id="sideB">
+			<section>
+				<h3>What is a Program</h3>
+				<article>A Program is a series of courses. Successful completion of all courses is required in order to abtain a Degree/Diploma or Certificate. Students are required to apply to the program in order to obtain their credential and apply to gradudate.</article>
+				<img src="images/schoolbus2.jpg" alt="schoolbus" width="272">
+				<p><a href="http://www.conestogac.on.ca/fulltime/computer-programmer-analyst">Read more >></a></p>
+			</section>
+		</aside>
+	  <footer>
+		<p>&copy; 2017 CPA Programs - Created by Moon</p>
+	  </footer>
+	</body>
+</html>
